d0626078adcfbe1d9dd8096b0e0ce030d50f705c
Grace <huang.yahui.grace@gmail.com>
2022-04-09 15:14:55 +0800

    Step 1: Project Setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,37 @@
 module.exports = {
-  prefix: '',
-  mode: 'jit',
-  content: [
-    './src/**/*.{html,ts}',
-  ],
-  darkMode: 'media', // or 'media' or 'class'
+  prefix: "",
+  mode: "jit",
+  content: ["./src/**/*.{html,ts}"],
+  purge: {
+    enabled: true,
+    content: [
+      "./src/**/*.{html,ts}",
+      "./projects/app2-restaurant/src/**/*.{html,ts}",
+      "./projects/app3-orders/src/**/*.{html,ts}",
+    ],
+  },
+  darkMode: "media", // or 'media' or 'class'
   theme: {
     fontFamily: {
-      sans: ['Helvetica Neue', 'Helvetica','Arial','sans-serif']
+      sans: ["Helvetica Neue", "Helvetica", "Arial", "sans-serif"],
     },
     extend: {
       colors: {
-        'picton-blue': {
-          light: '#337AB7',
-          DEFAULT: '#337AB7',
-          dark: '#14b367',
+        "picton-blue": {
+          light: "#337AB7",
+          DEFAULT: "#337AB7",
+          dark: "#14b367",
         },
-        'nav-hover-blue': {
-          DEFAULT: '#286090',
+        "nav-hover-blue": {
+          DEFAULT: "#286090",
         },
-        'nav-menu-active':{
-          DEFAULT: '#1d4568',
+        "nav-menu-active": {
+          DEFAULT: "#1d4568",
         },
-        'new': {
-          DEFAULT: '#D9EDF7',
+        new: {
+          DEFAULT: "#D9EDF7",
         },
-      }
+      },
     },
   },
   corePlugins: {
@@ -34,5 +40,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/forms'),require('@tailwindcss/typography')],
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
 };
